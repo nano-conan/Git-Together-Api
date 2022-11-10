ba42372858d0a98c4beedcff66b1b1aa67f63ea1
tinyconan <conan@bids.ca>
2022-11-10 16:58:19 -0400

    removing unused routes

--- routes/api.php
+++ routes/api.php
@@ -30,9 +30,3 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
-
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
