9584d6440a6c81e4ca0647577ed6c399b7af60cb
tinyconan <conan@bids.ca>
2022-11-24 11:33:40 -0400

    adding profile db migration + routes

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // ??
+        // Maybe for administrative purposes?
+
+        return Profile::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Profile::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $profile = Profile::find($id);
+        $profile->update($request->all());
+
+        return $profile;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,16 +18,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//Public Routes
+// Public Routes
+
 Route::post('/login', [AuthController::class,'login']);
 Route::post('/register', [AuthController::class,'register']);
-Route::get('/projects/search/{name}', [ProjectController::class, 'search']);
-Route::get('/projects', [ProjectController::class, 'index']);
 
-//Protected Routes
+/*
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
+*/
+
+// Protected Routes
+
 Route::middleware(['auth:sanctum'])->group(function () {
+    // Profile CRUD Routes
+    Route::resource('profile', ProfileController::class);
+
+    // Project Routes
+    Route::resource('projects', ProjectController::class);
+    Route::get('/projects/search/{name}', [ProjectController::class, 'search']);
+
+    /*
     Route::post('/projects', [ProjectController::class, 'store']);
     Route::put('/projects/{id}', [ProjectController::class, 'update']);
     Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    */
 });
